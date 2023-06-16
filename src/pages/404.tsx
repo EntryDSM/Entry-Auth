7c128228476107a39c7e53d68abe38d0b14a9b81
paperdeer <tjrgus0713@gmail.com>
2023-06-16 13:37:02 +0900

    feat: 비정상적인 경로로 접근시 login 페이지로 redirect

---
+++ src/pages/404.tsx
@@ -0,0 +1,10 @@
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router';
+
+export const NotFound = () => {
+  const navigate = useNavigate();
+  useEffect(() => {
+    navigate('/login');
+  }, [navigate]);
+  return null;
+};
