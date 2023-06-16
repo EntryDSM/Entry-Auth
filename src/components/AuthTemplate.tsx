210af331e4af6ee60dfa934d8511958f18666672
paperdeer <tjrgus0713@gmail.com>
2023-06-16 13:41:47 +0900

    feat: 각 페이지에서 사용하는 화면 컴포넌트화

--- src/components/AuthTemplate.tsx
+++ src/components/AuthTemplate.tsx
@@ -3,16 +3,29 @@ import { BackgroundImage } from '../assets/Background';
 
 interface PropsType {
   children: React.ReactNode;
+  title: string;
+  withUnderLine?: boolean;
   width?: number;
+  padding?: string;
 }
 
-export const AuthTemplate = ({ children, width = 500 }: PropsType) => {
+export const AuthTemplate = ({
+  children,
+  width = 500,
+  title,
+  withUnderLine,
+  padding = '50px',
+}: PropsType) => {
   return (
     <_Wrapper>
       <_Background>
         <BackgroundImage />
       </_Background>
-      <_Box width={width}>{children}</_Box>
+      <_Box width={width} padding={padding}>
+        <_Title>{title}</_Title>
+        {withUnderLine && <_Divider />}
+        {children}
+      </_Box>
     </_Wrapper>
   );
 };
@@ -23,6 +36,7 @@ const _Wrapper = styled.section`
   display: flex;
   justify-content: center;
   align-items: center;
+  background-color: white;
 `;
 
 const _Background = styled.div`
@@ -34,13 +48,25 @@ const _Background = styled.div`
 
 const _Box = styled.div<{
   width: number;
+  padding: string;
 }>`
   z-index: 2;
   width: 500px;
-  padding: 50px;
+  padding: ${(props) => props.padding};
   background-color: #ffffff;
-  padding: 50px;
   box-sizing: border-box;
   box-shadow: 0px 0px 8px rgba(0, 0, 0, 0.25);
   border-radius: 8px;
+`;
+
+const _Title = styled.h1`
+  ${({ theme }) => theme.font.header2};
+  text-align: center;
+`;
+
+const _Divider = styled.hr`
+  width: 50px;
+  height: 2px;
+  background-color: ${({ theme }) => theme.color.orange300};
+  margin: 20px auto 0 auto;
 `;
