6950ac50b9ff7baa45d85d323ed7b110c3303df7
paperdeer <tjrgus0713@gmail.com>
2023-06-21 23:01:04 +0900

    feat: favicon 빌드되게

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -1,6 +1,11 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 const path = require('path');
 const webpack = require('webpack');
+const fs = require('fs');
+
+const appDirectory = fs.realpathSync(process.cwd());
+const resolveApp = (relativePath) => path.resolve(appDirectory, relativePath);
 
 module.exports = {
   entry: `${path.resolve(__dirname, '../src')}/index.tsx`,
@@ -23,6 +28,17 @@ module.exports = {
     }),
     new webpack.ProvidePlugin({
       React: 'react',
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: resolveApp('public'),
+          to: '.',
+          globOptions: {
+            ignore: ['*/**/index.html'],
+          },
+        },
+      ],
     }),
   ],
   resolve: {
