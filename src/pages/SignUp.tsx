b7b86260e943a9bfaf8223ff044751eddbc21704
kimjh11130 <kimjh11130@naver.com>
2023-08-30 19:01:13 +0900

    fix: 회원가입 구분 추가

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,16 +1,17 @@
 import { RedirectURL } from '@/apis/login';
 import { useVerifyUserInfo } from '@/apis/verify';
 import { AuthTemplate } from '@/components/AuthTemplate';
-import { GoToAuthorization } from '@/components/GoToAuthorization';
+import DivideSignup from '@/components/DivideSignup';
 import { InsertUserInfo } from '@/components/InsertUserInfo';
 import { OnAuthorization } from '@/components/OnAuthorization';
 import { useToken } from '@/hooks/useToken';
 import { removeLocalStorageItem } from '@/utils/localstorage';
-import { useEffect, useMemo } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 
 export const SignUp = ({ redirectURL }: RedirectURL) => {
   const { token } = useToken();
   const { getUserData } = useVerifyUserInfo(token.mdl_tkn);
+  const [isStudent, setIsStudent] = useState(false);
 
   useEffect(() => {
     removeLocalStorageItem('isVerified');
@@ -19,16 +20,16 @@ export const SignUp = ({ redirectURL }: RedirectURL) => {
   const RenderedComponent = useMemo(() => {
     let component;
     if (getUserData.data?.data) {
-      component = <InsertUserInfo redirectURL={redirectURL} />;
+      component = (
+        <InsertUserInfo isStudent={isStudent} redirectURL={redirectURL} />
+      );
     } else if (token.mdl_tkn) {
       component = <OnAuthorization />;
     } else {
-      component = (
-        <GoToAuthorization text="본인 인증후 회원가입을 진행해 주세요" />
-      );
+      component = <DivideSignup setIsStudent={setIsStudent} />;
     }
     return component;
-  }, [redirectURL, getUserData.data?.data, token.mdl_tkn]);
+  }, [redirectURL, getUserData.data?.data, token.mdl_tkn, isStudent]);
 
   return <AuthTemplate title="회원가입">{RenderedComponent}</AuthTemplate>;
 };
