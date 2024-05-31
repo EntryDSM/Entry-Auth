104155147abd055e519ae70c5bc7618e4ecf05bf
dutexion <dutexion@gmail.com>
2024-05-31 16:03:40 +0900

    chore :: prod 환경에서도 devServer 추가

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,21 +1,30 @@
+const path = require('path');
 const webpack = require('webpack');
+
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
 module.exports = {
   mode: 'production',
-  devtool: 'inline-source-map',
-  devServer: {
-    open: false,
-    hot: true,
-    compress: true,
-    port: 3000,
-    historyApiFallback: true,
-    liveReload: true,
-  },
+  devtool: 'cheap-module-source-map',
   output: {
     filename: '[name].[contenthash].js',
-    publicPath: '/',
+    path: path.resolve(__dirname, '../dist'),
+    assetModuleFilename: 'static/media/[hash][ext][query]',
+    publicPath: './',
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(sa|sc|c)ss$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+    ],
   },
   plugins: [
+    new MiniCssExtractPlugin(),
     new webpack.EnvironmentPlugin({
       NODE_ENV: 'production',
     }),
@@ -25,12 +34,36 @@ module.exports = {
       ),
     }),
   ],
-  module: {
-    rules: [
-      {
-        test: /\.(sa|sc|c)ss$/i,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
-      },
+  optimization: {
+    usedExports: true,
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          compress: {
+            drop_console: true,
+          },
+        },
+      }),
+      new CssMinimizerPlugin(),
     ],
+    splitChunks: {
+      chunks: 'all',
+    },
+  },
+  performance: {
+    hints: false,
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000,
+  },
+  devServer: {
+    historyApiFallback: true,
+    static: {
+      directory: path.resolve(__dirname, '../dist'),
+    },
+    open: true,
+    compress: true,
+    hot: true,
+    port: 3000,
   },
 };
